419efdd4ff4675cf2447704b5386597d3528c2a6
Maria Vilaró <maria@utopigstudio.com>
2025-03-20 20:36:28 +0100

    frontend_url as config, fix email external assets

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block; width:150px">
-            <img src="https://noode.netlify.app/images/logo.svg" class="logo" alt="Noode Logo" style="display: inline-block; width:100%">
+            <img src="{{ config('app.frontend_url') }}/images/logo.svg" class="logo" alt="Noode Logo" style="display: inline-block; width:100%">
         </a>
     </td>
 </tr>
