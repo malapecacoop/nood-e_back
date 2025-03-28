e457426d70b9dd98c60ee92a5339ff7cdc97fceb
Maria Vilaró <maria@utopigstudio.com>
2025-03-28 19:25:24 +0100

    refactor

--- app/Models/Recurrency.php
+++ app/Models/Recurrency.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -33,5 +34,71 @@ class Recurrency extends Model
     public function firstEvent()
     {
         return $this->belongsTo(Event::class);
+    }
+
+    public function generateEvents()
+    {
+        // if just created, generate events from the first event date
+        if ($this->events()->count() == 1) {
+            $fromEvent = $this->firstEvent;
+        }
+        // if there are already events created, we are updating, generate events from the last event date
+        else {
+            $fromEvent = $this->events()->orderBy('start', 'desc')->first();
+        }
+
+        $dateRecurrencyEnd = $this->end;
+        $maxRecurrencyDate = Carbon::now()->addDays(Recurrency::DAYS_GENERATE);
+        if (!$dateRecurrencyEnd || $dateRecurrencyEnd->isAfter($maxRecurrencyDate)) {
+            $dateRecurrencyEnd = $maxRecurrencyDate;
+        }
+        $dateRecurrencyEnd->addDay();
+
+        $addPeriodMethod = $this->getCarbonMethod($this->type);
+
+        $events = [];
+
+        $dateStart = $fromEvent->start;
+        $dateEnd = $fromEvent->end;
+        $dateStart->$addPeriodMethod();
+        $dateEnd->$addPeriodMethod();
+
+        while ($dateStart->isBefore($dateRecurrencyEnd)) {
+            $events[] = [
+                'title' => $fromEvent->title,
+                'description' => $fromEvent->description,
+                'meet_link' => $fromEvent->meet_link,
+                'author_id' => $fromEvent->author_id,
+                'room_id' => $fromEvent->room_id,
+                'start' => $dateStart->format('Y-m-d H:i:s'), //this is needed to freeze the date (because the object $dateStart changes in each iteration)
+                'end' => $dateEnd->format('Y-m-d H:i:s'), // idem
+                'recurrency_id' => $this->id,
+            ];
+
+            $dateStart->$addPeriodMethod();
+            $dateEnd->$addPeriodMethod();
+        }
+        $this->events()->createMany($events);
+
+        $members = $fromEvent->members->pluck('id')->toArray();
+        foreach ($this->events as $event) {
+            $event->members()->sync($members);
+        }
+    }
+
+    private function getCarbonMethod($type)
+    {
+        switch ($type) {
+            case Recurrency::TYPE_DAY:
+                return 'addDay';
+            case Recurrency::TYPE_WEEK:
+                return 'addWeek';
+            case Recurrency::TYPE_MONTH:
+                return 'addMonthNoOverflow';
+            case Recurrency::TYPE_YEAR:
+                return 'addYear';
+            default:
+                throw new \InvalidArgumentException('Invalid recurrency type');
+        }
     }
 }
--- app/Observers/RecurrencyObserver.php
+++ app/Observers/RecurrencyObserver.php
@@ -4,103 +4,29 @@ namespace App\Observers;
 
 use App\Models\Event;
 use App\Models\Recurrency;
-use Carbon\Carbon;
 
 class RecurrencyObserver
 {
     public function updated(Recurrency $recurrency)
     {
         if ($recurrency->isDirty('first_event_id') && $recurrency->first_event_id) {
-            $dateRecurrencyEnd = $recurrency->end;
-            $maxRecurrencyDate = Carbon::now()->addDays(Recurrency::DAYS_GENERATE);
-            if (!$dateRecurrencyEnd || $dateRecurrencyEnd->isAfter($maxRecurrencyDate)) {
-                $dateRecurrencyEnd = $maxRecurrencyDate;
-            }
-
-            $dateRecurrencyEnd->addDay();
-
-            $this->generateEvents($recurrency, $recurrency->firstEvent, $dateRecurrencyEnd);
+            $recurrency->generateEvents();
         }
 
         if ($recurrency->isDirty('end')) {
             $oldEnd = $recurrency->getOriginal('end');
-            $dateRecurrencyEnd = $recurrency->end;
-            $maxRecurrencyDate = Carbon::now()->addDays(Recurrency::DAYS_GENERATE);
-            if (!$dateRecurrencyEnd || $dateRecurrencyEnd->isAfter($maxRecurrencyDate)) {
-                $dateRecurrencyEnd = $maxRecurrencyDate;
+            $newEnd = $recurrency->end;
+            if ($newEnd) {
+                $newEnd->addDay();
             }
-            $dateRecurrencyEnd->addDay();
-            if (!$oldEnd || $oldEnd->isAfter($maxRecurrencyDate)) {
-                $oldEnd = $maxRecurrencyDate;
-            }
-            $oldEnd->addDay();
 
-            if ($dateRecurrencyEnd->isBefore($oldEnd)) {
+            if (!$oldEnd || ($newEnd && $newEnd->isBefore($oldEnd))) {
                 Event::where('recurrency_id', $recurrency->id)
-                    ->where('start', '>=', $dateRecurrencyEnd)
+                    ->where('start', '>', $newEnd)
                     ->delete();
             } else {
-                $lastEvent = Event::where('recurrency_id', $recurrency->id)
-                    ->orderBy('start', 'desc')
-                    ->first();
-
-                $this->generateEvents($recurrency, $lastEvent, $dateRecurrencyEnd);
+                $recurrency->generateEvents();
             }
         }
-    }
-
-    private function generateEvents(
-        Recurrency $recurrency,
-        Event $event,
-        Carbon $recurrencyEnd
-    ): void
-    {
-        switch ($recurrency->type) {
-            case Recurrency::TYPE_DAY:
-                $addPeriodFunction = 'addDay';
-                break;
-            case Recurrency::TYPE_WEEK:
-                $addPeriodFunction = 'addWeek';
-                break;
-            case Recurrency::TYPE_MONTH:
-                $addPeriodFunction = 'addMonthNoOverflow';
-                break;
-            case Recurrency::TYPE_YEAR:
-                $addPeriodFunction = 'addYear';
-                break;
-            default:
-                throw new \InvalidArgumentException('Invalid recurrency type');
-        }
-
-        $events = [];
-
-        $dateStart = $event->start;
-        $dateEnd = $event->end;
-
-        $dateStart->$addPeriodFunction();
-        $dateEnd->$addPeriodFunction();
-
-        while ($dateStart->isBefore($recurrencyEnd)) {
-            $events[] = [
-                'title' => $event->title,
-                'description' => $event->description,
-                'meet_link' => $event->meet_link,
-                'author_id' => $event->author_id,
-                'room_id' => $event->room_id,
-                'start' => $dateStart->format('Y-m-d H:i:s'),
-                'end' => $dateEnd->format('Y-m-d H:i:s'),
-                'recurrency_id' => $recurrency->id,
-            ];
-            $dateStart->$addPeriodFunction();
-            $dateEnd->$addPeriodFunction();
-        }
-
-        $recurrency->events()->createMany($events);
-
-        $members = $event->members->pluck('id')->toArray();
-        foreach ($recurrency->events as $event) {
-            $event->members()->sync($members);
-        }
-    }
-    
+    }    
 }
